2553b60520259097ca613602585af6d799f8b31a
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-09-17 11:58:18 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let number=1111;
+let StringOne='1212';
+let StringTwo='hello';
+var numstring=Number(StringOne)
+console.log(numstring);
+
+//no the string cannot be converted into number because it carry words
+var numstring_one=Number(StringTwo);
+console.log(numstring_one);
